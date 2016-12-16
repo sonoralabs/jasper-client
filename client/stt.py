5f119486028bd84792aa4e3b9518907773d50395
Yoann Ciabaud <yoann@sonora.io>
2016-12-16 22:35:23 +0100

    Support de google cloud speech

--- client/stt.py
+++ client/stt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8-*-
+import base64
+from googleapiclient import discovery
 import os
 import wave
 import json
@@ -410,6 +412,182 @@ class GoogleSTT(AbstractSTTEngine):
                 raise ValueError('Nothing has been transcribed.')
             results = [alt['transcript'] for alt
                        in response['result'][0]['alternative']]
+        except ValueError as e:
+            self._logger.warning('Empty response: %s', e.args[0])
+            results = []
+        except (KeyError, IndexError):
+            self._logger.warning('Cannot parse response.', exc_info=True)
+            results = []
+        else:
+            # Convert all results to uppercase
+            results = tuple(result.upper() for result in results)
+            self._logger.info('Transcribed: %r', results)
+        return results
+
+    @classmethod
+    def is_available(cls):
+        return diagnose.check_network_connection()
+
+class GoogleCloudSTT(AbstractSTTEngine):
+    """
+    Speech-To-Text implementation which relies on the Google Cloud Speech API.
+
+    This implementation requires a Google API key to be present in profile.yml
+
+    To obtain an API key:
+    1. Join the Chromium Dev group:
+       https://groups.google.com/a/chromium.org/forum/?fromgroups#!forum/chromium-dev
+    2. Create a project through the Google Developers console:
+       https://console.developers.google.com/project
+    3. Select your project. In the sidebar, navigate to "APIs & Auth." Activate
+       the Speech API.
+    4. Under "APIs & Auth," navigate to "Credentials." Create a new key for
+       public API access.
+    5. Add your credentials to your profile.yml. Add an entry to the 'keys'
+       section using the key name 'GOOGLE_SPEECH.' Sample configuration:
+    6. Set the value of the 'stt_engine' key in your profile.yml to 'google'
+
+
+    Excerpt from sample profile.yml:
+
+        ...
+        timezone: US/Pacific
+        stt_engine: googlecloud
+        keys:
+            GOOGLE_SPEECH: $YOUR_KEY_HERE
+
+    """
+
+    SLUG = 'googlecloud'
+    # [START authenticating]
+    DISCOVERY_URL = ('https://{api}.googleapis.com/$discovery/rest?'
+                 'version={apiVersion}')
+
+
+    def __init__(self, api_key=None, language='en-US'):
+        # FIXME: get init args from config
+        """
+        Arguments:
+        api_key - the public api key which allows access to Google APIs
+        """
+        self._logger = logging.getLogger(__name__)
+        self._request_url = None
+        self._language = None
+        self._api_key = None
+        self._http = requests.Session()
+	
+        self.language = language
+        self.api_key = api_key
+
+    @property
+    def request_url(self):
+        return self._request_url
+
+    @property
+    def language(self):
+        return self._language
+
+    @language.setter
+    def language(self, value):
+        self._language = value
+        self._regenerate_request_url()
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value):
+        self._api_key = value
+        self._regenerate_request_url()
+
+    def _regenerate_request_url(self):
+        if self.api_key and self.language:
+            query = urllib.urlencode({'key': self.api_key})
+            self._request_url = urlparse.urlunparse(
+                ('https', 'speech.googleapis.com', '/v1beta1/speech:syncrecognize', '',
+                 query, ''))
+        else:
+            self._request_url = None
+
+    # Application default credentials provided by env variable
+    # GOOGLE_APPLICATION_CREDENTIALS
+    def _get_speech_service(self):
+        #credentials = GoogleCredentials.get_application_default().create_scoped(
+        #    ['https://www.googleapis.com/auth/cloud-platform'])
+        #http = httplib2.Http()
+        #credentials.authorize(http)
+
+        return discovery.build(
+            'speech', 'v1beta1', developerKey=self.api_key, discoveryServiceUrl=self.DISCOVERY_URL)
+    # [END authenticating]
+
+    @classmethod
+    def get_config(cls):
+        # FIXME: Replace this as soon as we have a config module
+        config = {}
+        # HMM dir
+        # Try to get hmm_dir from config
+        profile_path = jasperpath.config('profile.yml')
+        if os.path.exists(profile_path):
+            with open(profile_path, 'r') as f:
+                profile = yaml.safe_load(f)
+                if 'keys' in profile and 'GOOGLE_SPEECH' in profile['keys']:
+                    config['api_key'] = profile['keys']['GOOGLE_SPEECH']
+        return config
+
+    def transcribe(self, fp):
+
+	#fp = open('/home/pi/test.wav', 'rb')
+
+        """
+        Performs STT via the Google Speech API, transcribing an audio file and
+        returning an English string.
+
+        Arguments:
+        audio_file_path -- the path to the .wav file to be transcribed
+        """
+
+        if not self.api_key:
+            self._logger.critical('API key missing, transcription request ' +
+                                  'aborted.')
+            return []
+        elif not self.language:
+            self._logger.critical('Language info missing, transcription ' +
+                                  'request aborted.')
+            return []
+
+        #wav = wave.open(fp, 'rb')
+        #frame_rate = wav.getframerate()
+        #wav.close()
+        data = {
+		'config': {
+		        'encoding':'LINEAR16',
+		        'sampleRate': 16000,
+		        'languageCode': self.language,
+			'maxAlternatives': 5,
+			'speechContext': {
+				'phrases': ['Lola', 'TV', 'on', 'off']
+			}
+		},
+		'audio': {
+			'content': base64.b64encode(fp.read()).decode('UTF-8')
+		}
+	}
+	service = self._get_speech_service()
+	service_request = service.speech().syncrecognize(body=data)
+        # [END construct_request]
+        # [START send_request]
+        response = service_request.execute()
+        print(json.dumps(response))
+        # [END send_request]
+
+        try:
+            if not 'results' in response or len(response['results']) == 0:
+                # Response result is empty
+                raise ValueError('Nothing has been transcribed.')
+            results = [alt['transcript'] for alt
+                       in response['results'][0]['alternatives']]
         except ValueError as e:
             self._logger.warning('Empty response: %s', e.args[0])
             results = []
